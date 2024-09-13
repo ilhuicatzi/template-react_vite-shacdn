e5e1e23b5f988cb3051a84c89cf91c4bda28c664
Gonzalo Ilhuicatzi <g.ilhuicatzi@gmail.com>
2024-09-13 15:40:26 -0600

    Refactor CodeString component to improve readability and accessibility

--- src/components/code/CodeString.tsx
+++ src/components/code/CodeString.tsx
@@ -6,13 +6,13 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-function CodeString({code}:{code:string}) {
+function CodeString({ code }: { code: string }) {
   const copyToClipboard = () => {
     navigator.clipboard.writeText(code);
   };
   return (
-    <pre className="dark:bg-zinc-900 bg-zinc-100 my-2 rounded-md flex justify-between w-full max-w-xl">
-      <code className="font-inconsolata text-sm select-all pl-4 py-2 text-zinc-800 dark:text-zinc-100">
+    <pre className="dark:bg-zinc-900 bg-zinc-100 my-2 rounded-md flex justify-between w-full max-w-2xl">
+      <code className="font-inconsolata font-medium select-all pl-4 py-3 text-zinc-800 dark:text-zinc-100">
         {code}
       </code>
       <div className="flex justify-end items-start">
@@ -20,6 +20,7 @@ function CodeString({code}:{code:string}) {
           <Tooltip>
             <TooltipTrigger asChild>
               <button
+                aria-label="Copy code"
                 onClick={copyToClipboard}
                 className="bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700 p-1 rounded-md"
               >
